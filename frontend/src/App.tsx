386ae3555b9366c958b76bb3ee093d98d24ebc88
Joshua Gierend <josh-kre@gmx.net>
2023-11-05 16:03:45 +0100

    styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ axios.defaults.baseURL = "http://localhost:5000";
 function App() {
   return (
     <div className="App">
+      <h1 className="HeaderText">Model Fusion Sandbox</h1>
       <ModelFusionTest></ModelFusionTest>
     </div>
   );
